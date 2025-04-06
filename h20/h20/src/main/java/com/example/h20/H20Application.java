a2b7407c0aafd4347c2e7ea730a985f60bbece90
truong220 <truong210244@hnet.edu.vn>
2025-04-06 14:22:19 +0700

    hai

---
+++ h20/h20/src/main/java/com/example/h20/H20Application.java
@@ -0,0 +1,13 @@
+package com.example.h20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H20Application.class, args);
+	}
+
+}
---
+++ h20/h20/src/test/java/com/example/h20/H20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.h20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
